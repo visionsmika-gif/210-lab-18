a2a40e7b8503088c4f821e7f7157443c3b24d2d4
visionsmika-gif <visionsmika@gmail.com>
2025-10-02 15:49:09 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/* INSTRUCTIONS:
+A movie reviewer is a person who watches a film, rates how good it is, and provides comments.
+
+Write a C++ program which could be used to efficiently store these reviews by reviewers. Implement this in a linked list in which each node contains a rating and a comment.
+
+You'll need to write code that adds nodes to the tail of the linked list. The sample code in this module contains the code to add nodes to the head of the linked list.
+
+At program startup, ask the user which mode to use, new nodes at the head or the tail of the linked list.
+
+Obtain two pieces of data from the user, the rating and the comments. Store these in the linked list.
+
+After all reviews have been input, traverse the linked list to output the data and calculate/output the average review.*/
